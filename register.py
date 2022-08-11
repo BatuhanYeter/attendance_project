3b00947d9457d8e0546ca84f91aca9f0d84a8d36
Batuhan <batuhanyeter325@gmail.com>
2022-08-11 15:34:31 +0300

    register updated/fixed

--- register.py
+++ register.py
@@ -4,30 +4,62 @@ import numpy as np
 import os
 from datetime import datetime
 import db
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 from tkinter import *
 
+def registerGUI():
+    window = Tk()
+    window.geometry('400x400')
 
-print("Enter your first name:")
-firstName = input()
-print("Enter your lastname:")
-lastName = input()
-print("Enter your age:")
-age = input()
+    window.title("Attendance App")
 
-root = Tk()
-root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+    lblFName = Label(window, text="First Name")
+    lblLName = Label(window, text="Last Name")
+    lblAge = Label(window, text="Age")
 
-img = cv2.imread(f'{root.filename}')
-cv2.imshow("photo", img)
-cv2.waitKey(0)
+    inputFirstName = Entry(window,width=10)
+    inputLastName = Entry(window,width=10)
+    inputAge = Entry(window,width=10)
 
-path = "./images"
- 
-status = cv2.imwrite(f'{path}/{firstName+"_"+lastName}.jpg', img)
+    btnRegister = Button(window, text="Register and choose a picture", command=lambda:onClickRegister(inputFirstName.get(), inputLastName.get(), inputAge.get()))
+    # btnPicture = Button(window, text="Choose a picture", command=lambda:onClickPicture(inputFirstName.get(), inputLastName.get()))
 
-if status == True:
-    db.create_user(lastName=lastName, firstName=firstName, age=age)
-    print("welcome to the team")
+    lblFName.grid(column=0, row=0)
+    lblLName.grid(column=0, row=1)
+    lblAge.grid(column=0, row=2)
 
+    inputFirstName.grid(column=1, row=0)
+    inputLastName.grid(column=1, row=1)
+    inputAge.grid(column=1, row=2)
+    
+    # btnPicture.grid(column=1, row=3)
+    btnRegister.grid(column=1, row=4)
+    
+    window.mainloop()
 
+def onClickRegister(firstName,lastName,age):
+    root = Tk()
+    root.wm_withdraw()
+    root.filename = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+
+    img = cv2.imread(f'{root.filename}')
+    # cv2.imshow("photo", img)
+    # cv2.waitKey(0)
+
+    path = "./images"
+    
+    status = cv2.imwrite(f'{path}/{firstName+"_"+lastName}.jpg', img)
+    if status == True:
+        if lastName != "" and firstName != "" and age != "":
+            db.create_user(lastName=lastName, firstName=firstName, age=age)
+            messagebox.showinfo("Registered", f'Welcome aboard, {firstName}!')
+            root.destroy()
+        else:
+            messagebox.showerror("Empty information", "Please fill all the requirements.")
+            root.destroy()
+    else:
+        messagebox.showerror("Error", "Something went wrong!")
+        root.destroy()
+
+        
+registerGUI()
